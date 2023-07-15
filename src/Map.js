5925e2e406956ea1884503d67686989877b520bb
Arghadip Chatterjee <114013720+Arghadip-Chatterjee@users.noreply.github.com>
2023-07-15 22:52:41 +0530

    Fixed

--- src/Map.js
+++ src/Map.js
@@ -1,57 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import mapboxgl from 'mapbox-gl';
-// import 'mapbox-gl/dist/mapbox-gl.css';
-// import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
-// import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
-// import RideSelector from './RideSelector';
-// import './Map.css';
-
-// mapboxgl.accessToken = 'pk.eyJ1IjoiYXJnaGFkaXAiLCJhIjoiY2xobHdoZmc3MTV1aTNqbnhmbDgydjU4eSJ9.dQeMUNENDkyWKH-Jwody_A';
-
-// const Map = () => {
-//   const [distance, setDistance] = useState(null);
-
-//   useEffect(() => {
-//     const map = new mapboxgl.Map({
-//       container: 'map',
-//       style: 'mapbox://styles/mapbox/streets-v12',
-//       center: [-79.4512, 43.6568],
-//       zoom: 13
-//     });
-
-//     const directions = new MapboxDirections({
-//       accessToken: mapboxgl.accessToken
-//     });
-
-//     map.addControl(directions, 'top-left');
-
-//     directions.on('route', (e) => {
-//       const newDistance = e.route[0].distance; // meters
-//       const distanceInMiles = newDistance * 0.001; // convert meters to km
-//       setDistance(distanceInMiles);
-//     });
-
-//     return () => {
-//       map.remove();
-//     };
-//   }, []);
-
-//   return (
-//     <div className='background'>
-//       <div className="map-container">
-//         <div id="map" className="map" />
-//       </div>
-
-//       <div className='ride-selector-container'>
-//         <RideSelector distance={distance} />
-//       </div>
-//     </div>
-//   );
-
-// };
-
-// export default Map;
-
 import React, { useEffect, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet-routing-machine';
@@ -61,6 +7,7 @@ import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import RideSelector from './RideSelector';
 import ChatSonicChat from './writesonic'; // Import your ChatSonic component
 import './Map.css'
+import Chatbot from './writesonic';
 
 const Map = () => {
   const [pickup, setPickup] = useState('');
@@ -71,8 +18,8 @@ const Map = () => {
   const [pickupMarker, setPickupMarker] = useState(null);
   const [dropoffMarker, setDropoffMarker] = useState(null);
   const [taxiMarker, setTaxiMarker] = useState(null);
-  const [showChatSonic, setShowChatSonic] = useState(true); // State to control the visibility of ChatSonic
-  const [chatSonicOutput, setChatSonicOutput] = useState(''); // State to store ChatSonic output
+  const [locationAccessed, setLocationAccessed] = useState(false); // Track whether location has been accessed
+  const [currentLocation, setCurrentLocation] = useState(null);
 
   useEffect(() => {
     const initializeMap = () => {
@@ -80,45 +27,69 @@ const Map = () => {
       if (container != null) {
         container._leaflet_id = null;
       }
-  
+
       const newMap = L.map('map', {
         center: [22.5626, 88.3630],
         zoom: 11,
-        dragging: true, // Enable dragging
-        scrollWheelZoom: true, // Enable scroll wheel zoom
+        dragging: true,
+        scrollWheelZoom: true,
       });
-  
+
       const mapLink = "<a href='http://openstreetmap.org'>OpenStreetMap</a>";
       L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: 'Leaflet &copy; ' + mapLink + ', contribution',
         maxZoom: 18,
       }).addTo(newMap);
-  
+
       setMap(newMap);
     };
-  
+
     initializeMap();
   }, []);
 
+  useEffect(() => {
+    // Retrieve the current location from localStorage when the component mounts
+    const storedLocation = localStorage.getItem('currentLocation');
+    if (storedLocation) {
+      setCurrentLocation(JSON.parse(storedLocation));
+    }
+  }, []);
 
   const handleLocationAccess = () => {
     if (!map) return;
-
-    map.locate({ setView: true, maxZoom: 16 });
-
-    map.on('locationfound', (e) => {
-      const currentLocation = e.latlng;
-      const currentLatLng = L.latLng(currentLocation.lat, currentLocation.lng);
-      localStorage.setItem("currentLocation", JSON.stringify(currentLatLng));
-
-      setPickup('Current Location');
-
-      if (dropoff.trim() !== '') {
-        calculateRoute(currentLatLng);
+  
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+  
+        const currentLatLng = L.latLng(latitude, longitude);
+        localStorage.setItem('currentLocation', JSON.stringify(currentLatLng));
+  
+        setPickup('Current Location');
+        setLocationAccessed(true);
+        setCurrentLocation(currentLatLng); // Update the current location state
+  
+        if (dropoff.trim() !== '') {
+          calculateRoute(currentLatLng);
+        }
+  
+        const customIcon = L.icon({
+          iconUrl: 'https://vectorified.com/images/google-maps-marker-icon-38.png',
+          iconSize: [32, 32],
+        });
+  
+        const marker = L.marker([latitude, longitude], { icon: customIcon }).addTo(map);
+  
+        map.setView([latitude, longitude], 13);
+      },
+      (error) => {
+        console.error('Error getting current location:', error);
       }
-    });
+    );
   };
-
+  
+  
+  
   const calculateRoute = (pickupLatLng) => {
     const geocoder = L.Control.Geocoder.nominatim();
 
@@ -204,19 +175,13 @@ const Map = () => {
     }
   };
 
-  const handleToggleChatSonic = () => {
-    setShowChatSonic(!showChatSonic);
-  };
-
-  const handleChatSonicSubmit = (output) => {
-    setChatSonicOutput(output);
-  };
+  
   
   return (
-    <div className='background'>
+    <div className="background">
     <div id="map" className="map"></div>
     <div className="distance">
-      <h3>Distance Traveled:</h3> 
+      <h3>Distance Traveled:</h3>
       <h4>{distance} km</h4>
     </div>
     <div className="input-container">
@@ -236,39 +201,26 @@ const Map = () => {
         className="input-field"
       />
       <br />
-      {/* <button onClick={handleLocationAccess} className="button">
+      <button onClick={handleLocationAccess} className="button">
         Location Access
-      </button> */}
+      </button>
+
+      {locationAccessed && (
+        <p className="location-message">Location has been accessed!</p>
+      )}
       <br />
       <button onClick={handleSearch} className="button">
         Search
       </button>
       <br />
-      <button onClick={handleToggleChatSonic} className="chat-sonic-button">
-        ChatSonic
-      </button>
-      <br />
-      {showChatSonic && <ChatSonicChat />} {/* Render ChatSonicChat component if showChatSonic is true */}
-      <div className="chat-sonic-output">
-        {/* Display ChatSonic output */}
-        {chatSonicOutput && (
-          <div>
-            <strong>ChatSonic Output:</strong> {chatSonicOutput}
-          </div>
-        )}
+
+      <div>
+        <Chatbot />
       </div>
     </div>
-    <RideSelector distance={distance} /> {/* Pass the distance as a prop */}
+    <RideSelector distance={distance} />
   </div>
   );
 };
 
 export default Map;
-
-
-
-
-
-
-
-
--- src/writesonic.js
+++ src/writesonic.js
@@ -1,84 +1,59 @@
 import React, { useState } from 'react';
-import tw from 'tailwind-styled-components';
+import axios from 'axios';
+import './writesonic.css'; // Import CSS file for styling
 
-const ChatWrapper = tw.div`bg-gray-300`;
-const Input = tw.input`bg-white`;
-
-const ChatSonicChat = () => {
+const Chatbot = () => {
   const [userInput, setUserInput] = useState('');
-  const [chatHistory, setChatHistory] = useState([]);
+  const [response, setResponse] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    var latlong = localStorage.getItem("currentLocation");
-    // Make API request to ChatSonic
-    if (userInput.search("nearest") > 0) {
-      if (latlong) {
-        latlong = JSON.parse(latlong); 
-        setUserInput(userInput+`. My Current latitude is ${latlong.lat} and my longitude is ${latlong.lng}`);
-      }
-    }
-
-    const response = await fetch('https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language=en', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-API-KEY': 'e50a69d5-e75a-434d-8b29-a14d66bd9a52'
-      },
-      body: JSON.stringify({ input_text: userInput, enable_google_results: true, enable_memory: true })
-    });
-
-    const data = await response.json();
-    const message = data.message;
-
-    setChatHistory([...chatHistory, { message: userInput, sent: true }, { message: message, sent: false }]);
-    setUserInput('');
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
   };
 
-  const convertHtmlToText = (html) => {
-    const tempElement = document.createElement("div");
-    tempElement.innerHTML = html;
-    return tempElement.textContent || tempElement.innerText || "";
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'GET',
+      url: 'https://chatgpt-api9.p.rapidapi.com/ask',
+      params: {
+        question: userInput
+      },
+      headers: {
+        'X-RapidAPI-Key': '1b43b998e8mshecf18b614780362p1be75ejsne18f9cebbd57',
+        'X-RapidAPI-Host': 'chatgpt-api9.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await axios.request(options);
+      setResponse(response.data.answer);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    // <div>
-    //   <div className="border-2 m-auto">
-    //     {/* Render chat history */}
-    //     {chatHistory.map((chat, index) => (
-    //       <div key={index}>
-    //         <p>
-    //           {chat.sent ? "User: " : "AI: "}
-    //           {convertHtmlToText(chat.message)}
-    //         </p>
-    //       </div>
-    //     ))}
-    //   </div>
-    //   <form onSubmit={handleSubmit}>
-    //     <Input type="text" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
-    //     <button type="submit">Send</button>
-    //   </form>
-    // </div>
-
-    <ChatWrapper>
-      <div className="border-2 m-auto">
-        {/* Render chat history */}
-        {chatHistory.map((chat, index) => (
-          <div key={index}>
-            <p>
-              {chat.sent ? "User: " : "AI: "}
-              {convertHtmlToText(chat.message)}
-            </p>
-          </div>
-        ))}
-      </div>
+    <div className="chatbot-container">
+      <h1 className="chatbot-title">Chatbot</h1>
       <form onSubmit={handleSubmit}>
-        <Input type="text" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
-        <button type="submit">Send</button>
+        <input
+          type="text"
+          value={userInput}
+          onChange={handleInputChange}
+          className="chatbot-input"
+          placeholder="Ask a question..."
+        />
+        <button type="submit" className="chatbot-submit-button">
+          Submit
+        </button>
       </form>
-    </ChatWrapper>
+      <div className="chatbot-response-container">
+        <h2 className="chatbot-response-title">Response:</h2>
+        <p className="chatbot-response">{response}</p>
+      </div>
+    </div>
   );
 };
 
-export default ChatSonicChat;
+export default Chatbot;
